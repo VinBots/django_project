83e9b8397190e045b9e4ca79744ad6abe1f54f2e
Vincent Manier <vincentmanier@hotmail.com>
2021-07-04 15:37:38 -0400

    integrate dash plotly4

--- django_project/settings.py
+++ django_project/settings.py
@@ -181,7 +181,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            'hosts': [('64.227.14.214', 6379),],
+            'hosts': [('127.0.0.1', 6379),],
         },
     },
 }
