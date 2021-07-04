a246614cae9f5514845d2c5ffb5717ffdd87feb6
Vincent Manier <vincentmanier@hotmail.com>
2021-07-04 18:20:21 -0400

    try a data table chart on a new page

--- django_project/dash_data_table.py
+++ django_project/dash_data_table.py
@@ -1,38 +1,17 @@
-from django_plotly_dash import DjangoDash
 import dash
-from dash.dependencies import Input, Output
 import dash_table
 import pandas as pd
+from django_plotly_dash import DjangoDash
 
-
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
-
-df[' index'] = range(1, len(df) + 1)
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/solar.csv')
 
 app = DjangoDash('data_table')
 
-PAGE_SIZE = 5
-
 app.layout = dash_table.DataTable(
-    id='datatable-paging',
-    columns=[
-        {"name": i, "id": i} for i in sorted(df.columns)
-    ],
-    page_current=0,
-    page_size=PAGE_SIZE,
-    page_action='custom'
+    id='table',
+    columns=[{"name": i, "id": i} for i in df.columns],
+    data=df.to_dict('records'),
 )
-
-
-@app.callback(
-    Output('datatable-paging', 'data'),
-    Input('datatable-paging', "page_current"),
-    Input('datatable-paging', "page_size"))
-def update_table(page_current,page_size):
-    return df.iloc[
-        page_current*page_size:(page_current+ 1)*page_size
-    ].to_dict('records')
-
 
 if __name__ == '__main__':
     app.run_server(debug=True)
