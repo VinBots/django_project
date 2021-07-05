80e7a688f03dd1c7029671c04b7038b73f1c17f9
Vincent Manier <vincentmanier@hotmail.com>
2021-07-05 11:56:47 -0400

    science-based targets

--- django_project/utilities.py
+++ django_project/utilities.py
@@ -3,12 +3,11 @@ import pandas as pd
 import numpy as np
 import os
 
-def get_data(sheetname, cols_to_use):
+def get_data(xlsx_path, sheetname, cols_to_use):
     
-    XLSX_PATH = os.path.join ('django_project/static/django_project', 'data', 'sp100_data.xlsx')
     
     return pd.read_excel(
-        XLSX_PATH,
+        xlsx_path,
         sheet_name=sheetname, 
         engine = 'openpyxl',
         usecols=cols_to_use
@@ -17,11 +16,21 @@ def get_data(sheetname, cols_to_use):
 
 def get_top_stats():
 
+    #Excel file path
+    xlsx_path = os.path.join ('django_project/static/django_project', 'data', 'sp100_data.xlsx')
+
     # Connect to the data source
-    all_data = get_data('company', ['Company Name', 'Science-Based Target? (Y/N)'])
+    all_data = get_data(
+        xlsx_path, 
+        'company', 
+        ['Company Name', 'Science-Based Target? (Y/N)']
+        )
 
     # query
+    yes = all_data[all_data['Science-Based Target? (Y/N)'] == 'Y']
+    no = all_data[all_data['Science-Based Target? (Y/N)'] == 'N']
 
     # return a list
+    sbt_pct = int(100 * len(yes) / (len(yes) + len(no)))
 
-    return [15, 25, 45, 54, 47]
+    return [15, 25, 45, sbt_pct, 47]
