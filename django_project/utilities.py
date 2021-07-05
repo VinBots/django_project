8631db26a52f90b9142be0e5c56358cba6724576
Vincent Manier <vincentmanier@hotmail.com>
2021-07-05 11:18:09 -0400

    science-based targets

--- django_project/utilities.py
+++ django_project/utilities.py
@@ -1,6 +1,24 @@
+
+import pandas as pd
+import numpy as np
+import os
+
+def get_data(sheetname, cols_to_use):
+    
+    XLSX_PATH = os.path.join ('/static/django_project', 'data', 'sp100_data.xlsx')
+    
+    return pd.read_excel(
+        XLSX_PATH,
+        sheet_name=sheetname, 
+        engine = 'openpyxl',
+        usecols=cols_to_use
+        )
+
+
 def get_top_stats():
 
     # Connect to the data source
+    all_data = get_data('company', ['Company Name', 'Science-Based Target? (Y/N)'])
 
     # query
 
