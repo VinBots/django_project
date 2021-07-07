5def1e8f822c33dd6ae34fe987c2e13b311b0dbf
Vincent Manier <vincentmanier@hotmail.com>
2021-07-07 06:59:14 -0400

    playground

--- django_project/settings.py
+++ django_project/settings.py
@@ -16,6 +16,8 @@ import netifaces
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
