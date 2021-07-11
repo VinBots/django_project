a33dd41c4721d7d3b40e4f1a2a5a005457357f1e
Vincent Manier <vincentmanier@hotmail.com>
2021-07-11 10:09:39 -0400

    proto design

--- django_project/urls.py
+++ django_project/urls.py
@@ -28,8 +28,7 @@ dashboards_names = [
     "django_project.dashboards.dashboard_ambition",
     "django_project.dashboards.dashboard_sciencebased",
     "django_project.dashboards.dashboard_momentum",
-    "django_project.dashboards.dashboard_playground",
-    "django_project.dashboards.dashboard_company"
+    "django_project.dashboards.dashboard_playground"
     ]
 
 for lib in dashboards_names:
