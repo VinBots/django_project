05ed05607c9e5286f73500e46e3b3b943b21b4c8
Vincent Manier <vincentmanier@hotmail.com>
2021-07-11 15:37:25 -0400

    proto design

--- django_project/dashboards/dashboard_benchmark.py
+++ django_project/dashboards/dashboard_benchmark.py
@@ -247,8 +247,8 @@ app = DjangoDash('performance_company_dashboard')
 
 # Design the app layout
 app.layout = html.Div([
-    html.P(id = "hidden-div-for-slug"),
-    html.Div(id = 'hidden-div-for-slug'),
+    html.P(id="hidden-div-for-slug"),
+    html.Div(id='hidden-div-for-slug'),
     html.Div('Hello')])
 
 if __name__ == '__main__':
