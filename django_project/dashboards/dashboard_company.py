23e32bc5f3d05694f9d8e07f53dd5cda98f0ef72
Vincent Manier <vincentmanier@hotmail.com>
2021-07-11 09:59:23 -0400

    proto design

--- django_project/dashboards/dashboard_company.py
+++ django_project/dashboards/dashboard_company.py
@@ -2,3 +2,10 @@
 from django_plotly_dash import DjangoDash
 
 app = DjangoDash('company_performance_dashboard')
+app.layout = html.Div([
+        html.Div([
+            html.Div([
+                html.Div(
+                    ["gg",
+                    "hh"]),
+                html.Div(["company_name"]),])])])
