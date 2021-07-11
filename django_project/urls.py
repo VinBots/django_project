f58767a5fcc1d6c61ff971982352601c7858fec0
Vincent Manier <vincentmanier@hotmail.com>
2021-07-11 09:44:13 -0400

    proto design

--- django_project/urls.py
+++ django_project/urls.py
@@ -28,9 +28,8 @@ dashboards_names = [
     "django_project.dashboards.dashboard_ambition",
     "django_project.dashboards.dashboard_sciencebased",
     "django_project.dashboards.dashboard_momentum",
-    "django_project.dashboards.dashboard_playground",
-    "django_project.dashboards.dashboard_benchmark"
-]
+    "django_project.dashboards.dashboard_playground"
+    ]
 
 for lib in dashboards_names:
     globals()[lib] = __import__(lib)
