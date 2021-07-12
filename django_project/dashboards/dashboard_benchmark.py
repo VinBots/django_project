a54d29d3eb3ccbfe1dae9a67a5091e745b7d3d0a
Vincent Manier <vincentmanier@hotmail.com>
2021-07-12 10:13:54 -0400

    try use session state

--- django_project/dashboards/dashboard_benchmark.py
+++ django_project/dashboards/dashboard_benchmark.py
@@ -19,7 +19,7 @@ app = DjangoDash(
 
 app.layout = html.Div(
         [html.Div('HELLO WORLD'),
-        dcc.Input(id = 'company_name' , value = ''),
+        dcc.Input(id = 'company_name', type = 'text', value = 'hello'),
         html.Div(id = 'another_name', children = 'output'),
         html.Div('HEllo AGAIN!'),
         ])
