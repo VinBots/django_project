08f54334085172306e5eed529097e38b356f46a5
Vincent Manier <vincentmanier@hotmail.com>
2021-07-11 21:39:06 -0400

    try pass variables to Django Dash app

--- django_project/dashboards/dashboard_benchmark.py
+++ django_project/dashboards/dashboard_benchmark.py
@@ -27,7 +27,7 @@ app.layout = html.Div(
     [Input(component_id = 'company_name', component_property = 'value')]
     )
 def display_output(value, **kwargs):
-    return value
+    return "call back done " + value
 
 """
 app.layout = html.Div([
