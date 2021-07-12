39019e497c91e4aaf10647c5e39a81c68ca478d7
Vincent Manier <vincentmanier@hotmail.com>
2021-07-11 20:54:04 -0400

    try pass variables to Django Dash app

--- django_project/dashboards/dashboard_benchmark.py
+++ django_project/dashboards/dashboard_benchmark.py
@@ -35,10 +35,9 @@ def display_output(value, **kwargs):
 app.layout = html.Div([
     html.H6("Change the value in the text box to see callbacks in action!"),
     html.Div(["Input: ",
-              dcc.Input(id='my-input', value='initial value', type='text')]),
+              Input(id='my-input', value='initial value', type='text')]),
     html.Br(),
     html.Div(id='my-output'),
-
 ])
 
 @app.callback(
