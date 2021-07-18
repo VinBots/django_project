cfe349782250c432195a1f9fbbd2c9a1001c09e4
Vincent Manier <vincentmanier@hotmail.com>
2021-07-18 13:12:31 -0400

    pass corp name

--- corporates/urls.py
+++ corporates/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<str:corp_name>/', views.corporates_search, name = 'corporates'),
+    path('<str:corp_name>/', views.corporates_search, name = 'corporates_search'),
     path('', views.home, name='corporate_home'),
     ]
