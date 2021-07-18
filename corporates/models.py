e6cfb2ae0470a578133a3ced87f003240ba255d8
Vincent Manier <vincentmanier@hotmail.com>
2021-07-18 15:21:04 -0400

    pass corp name

--- corporates/models.py
+++ corporates/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Corporate(models.Model):
     name = models.CharField(max_length=250)
+    filename = models.CharField(max_length=20)
 
     def __str__(self):
         return self.name
