b7661cc05c124a1eeac7ad7963ee30429ba35fe4
Vincent Manier <vincentmanier@hotmail.com>
2021-07-19 22:06:09 -0400

    fetch ghg data

--- corporates/utilities.py
+++ corporates/utilities.py
@@ -1,6 +1,9 @@
 import os
 from pathlib import Path
 from corporates.models import Corporate
+import pandas as pd
+import os
+
 
 BASE_DIR = os.path.join(Path(__file__).parent.parent, "django_project")
 DIR_TO_CORP_CHARTS_TEMPLATES = "templates/django_project/corporates/charts/html_exports/"
@@ -21,9 +24,34 @@ def check_validity(corp_name):
   return all(conditions)
 
 def get_ghg_xls(corp_name):
+    
+    #Excel file path
+    xlsx_path = os.path.join ('../django_project/static/django_project', 'data', 'sp100_data.xlsx')
+
+    # Connect to the data source
+    all_data = get_data(
+        xlsx_path, 
+        'GHG19', 
+        None,
+        )
+    
+    company_id = 4
+    corp_record =  all_data[all_data['company_id']==company_id]
+    corp_record_scope1 = corp_record['gross_total_scope1'].iloc[0]
+    
     ghg_dict = {
         '2017':{'scope1':"20", 'scope2':"10", 'scope3':"20",'total':"50"},
         '2018':{'scope1':"5", 'scope2':"15", 'scope3':"25",'total':"45"},
-        '2019':{'scope1':"9", 'scope2':"19", 'scope3':"29",'total':"57"}}
+        '2019':{'scope1':str(corp_record_scope1), 'scope2':"19", 'scope3':"29",'total':"57"}}
     
     return ghg_dict
+
+
+def get_data(xlsx_path, sheetname, cols_to_use):
+    
+    return pd.read_excel(
+        xlsx_path,
+        sheet_name=sheetname, 
+        engine = 'openpyxl',
+        usecols=cols_to_use
+        )
