8ee8c969aa4d909bc7a76b17ed56f26e50dd4175
Vincent Manier <vincentmanier@hotmail.com>
2021-07-19 20:58:22 -0400

    insert chart

--- corporates/parameters.py
+++ corporates/parameters.py
@@ -1,11 +1,12 @@
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-DIR_TEMPLATE = os.path.join(BASE_DIR, 'templates', 'django_project')
-DIR_TO_CORP_CHARTS_TEMPLATES = "django_project/corporates/charts/html_exports/"
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent.parent
+DIR_TO_CORP_CHARTS_TEMPLATES = "templates/django_project/corporates/charts/html_exports/"
 
 def get_path_to_bubble(filename):
     path = os.path.join(
-        DIR_TEMPLATE,
+        BASE_DIR,
         DIR_TO_CORP_CHARTS_TEMPLATES) + "bubble_intensity_"+ filename + ".html"
 
     if os.path.exists(path):
