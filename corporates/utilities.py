2136b34832eda6a50826d80be8b29e856f015835
Vincent Manier <vincentmanier@hotmail.com>
2021-07-20 21:56:31 -0400

    add records

--- corporates/utilities.py
+++ corporates/utilities.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from corporates.models import Corporate
 import pandas as pd
 import os
-
+import math
 
 BASE_DIR = os.path.join(Path(__file__).parent.parent, "django_project")
 DIR_TO_CORP_CHARTS_TEMPLATES = "templates/django_project/corporates/charts/html_exports/"
@@ -38,6 +38,9 @@ def get_ghg_xls(company_id):
             )
         corp_record =  all_data[all_data['company_id']==company_id]
         corp_record_data = corp_record[['gross_total_scope1', 'gross_scope2_calc', 'gross_total_scope3', 'total_scope']]
+        corp_record_data.apply(pd.to_numeric, errors='coerce')
+        #merged_df_sector = merged_df_sector.dropna()
+
         ghg_dict[year] = {
             'scope1':ghg_format(corp_record_data.iloc[0,0]),
             'scope2':ghg_format(corp_record_data.iloc[0,1]),
@@ -58,4 +61,7 @@ def get_data(xlsx_path, sheetname, cols_to_use):
         )
 
 def ghg_format(number):
-    return f'{float(number):,.0f}'
+    if math.isnan(number):
+        return "not found"
+    else:
+        return f'{float(number):,.0f}'
