2fc4e933807dbd82c8db854c73d14cb22a640089
VinBots <vincentmanier@hotmail.com>
2021-07-24 12:48:56 +0200

    updated charts

--- corporates/utilities.py
+++ corporates/utilities.py
@@ -11,9 +11,9 @@ DIR_TO_CORP_CHARTS_TEMPLATES = "templates/django_project/corporates/charts/html_
 def get_path_to_chart(company_id, chart_name):
     path = os.path.join(
         BASE_DIR,
-        DIR_TO_CORP_CHARTS_TEMPLATES),
+        DIR_TO_CORP_CHARTS_TEMPLATES,
         chart_name,
-        chart_name + str(company_id) + ".html"
+        chart_name + str(company_id) + ".html")
 
     if os.path.exists(path):
         return path    
