424601556da20ef20ee5fd3122bd3d53cf645115
VinBots <vincentmanier@hotmail.com>
2021-08-06 10:41:35 +0200

    try leaderboard

--- django_project/static/django_project/scripts/leaderboard.js
+++ django_project/static/django_project/scripts/leaderboard.js
@@ -1,7 +1,7 @@
 var tabs = document.querySelectorAll(".lboard_tabs ul li");
-var today = document.querySelector(".today");
-var month = document.querySelector(".month");
-var year = document.querySelector(".year");
+var level1 = document.querySelector(".level1");
+var level2 = document.querySelector(".level2");
+var level3 = document.querySelector(".level3");
 var items = document.querySelectorAll(".lboard_item");
 
 tabs.forEach(function(tab){
@@ -18,14 +18,14 @@ tabs.forEach(function(tab){
 			item.style.display = "none";
 		})
 
-		if(currenttab == "today"){
-			today.style.display = "block";
+		if(currenttab == "level1"){
+			level1.style.display = "block";
 		}
-		else if(currenttab == "month"){
-			month.style.display = "block";
+		else if(currenttab == "level2"){
+			level2.style.display = "block";
 		}
 		else{
-			year.style.display = "block";
+			level3.style.display = "block";
 		}
 
 	})
