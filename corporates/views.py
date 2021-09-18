6fd95906291306f2d57947553acc7635c21e2993
VinBots <vincentmanier@hotmail.com>
2021-09-18 18:14:08 +0200

    speed

--- corporates/views.py
+++ corporates/views.py
@@ -17,8 +17,7 @@ def corporates_search(request, corp_name=None):
   if check_validity(corp_name):
 
     selected_corp = Corporate.objects.get(name=corp_name)
-    #all_data = get_all_data()
-    all_data = {}
+    all_data = get_all_data()
 
     xls_corp = {
       'ghg': get_ghg(company_id=selected_corp.company_id, all_data = all_data['ghg_quant']),
