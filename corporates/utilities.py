85f6ed9661821eb9a55d6f7982a0ab7d579d60a9
VinBots <vincentmanier@hotmail.com>
2021-09-18 18:27:07 +0200

    speed

--- corporates/utilities.py
+++ corporates/utilities.py
@@ -227,7 +227,7 @@ def get_data(xlsx_path, sheetname, cols_to_use):
 def get_all_data():
     xlsx_path = os.path.join (BASE_DIR_XL_DB, 'sp100.xlsx')
     sheet_names =['ghg_quant', 'corp_scores', 'score_summary', 'targets_quant']
-    sheet_names =['ghg_quant']
+    sheet_names =['ghg_quant', 'corp_scores'] 
 
     return pd.read_excel(
         xlsx_path, 
