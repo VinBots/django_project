fd7f1c18182007f9b33855d5024b2badfbe5e41b
VinBots <vincentmanier@hotmail.com>
2021-09-18 17:13:56 +0200

    speed

--- corporates/utilities.py
+++ corporates/utilities.py
@@ -38,36 +38,6 @@ def check_validity(corp_name):
   conditions = [cond1, cond2]
   return all(conditions)
 
-def get_ghg_xls(company_id):
-    ghg_dict={}
-    #Excel file path
-    #xlsx_path = os.path.join (BASE_DIR, 'static/django_project', 'data', 'sp100_data.xlsx')
-    '''
-    xlsx_path = os.path.join (BASE_DIR_XL_DB, 'sp100.xlsx')
-    year_ghgsheet = [('2017','GHG17'), ('2018','GHG18'), ('2019','GHG19')]
-
-    # Connect to the data source
-    for year, ghgsheet in year_ghgsheet:
-        all_data = get_data(
-            xlsx_path, 
-            ghgsheet, 
-            None,
-            )
-        corp_record =  all_data[all_data['company_id']==company_id]
-        if corp_record.empty:
-            corp_record_data = corp_record[['gross_total_scope1', 'gross_scope2_calc', 'gross_total_scope3', 'total_scope']]
-            corp_record_data = corp_record_data.apply(pd.to_numeric, errors='coerce')
-            #merged_df_sector = merged_df_sector.dropna()
-
-            ghg_dict[year] = {
-                'scope1':ghg_format(corp_record_data.iloc[0,0]),
-                'scope2':ghg_format(corp_record_data.iloc[0,1]),
-                'scope3':ghg_format(corp_record_data.iloc[0,2]),
-                'total':ghg_format(corp_record_data.iloc[0,3]),
-                }
-    '''
-    return ghg_dict
-
 def get_score_data(company_id):
     xlsx_path = os.path.join (BASE_DIR_XL_DB, 'sp100.xlsx')
 
