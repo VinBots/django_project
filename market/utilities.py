78d8bef895fd2bb602d140f273d1519cc4bd8619
Vincent Manier <vincentmanier@hotmail.com>
2021-12-31 16:20:43 -0500

    design market data table

--- market/utilities.py
+++ market/utilities.py
@@ -17,27 +17,27 @@ def get_market_stats(corp_number = None, top_rank=True):
     
     
     """
-
     # Connect to the data source
     all_data = get_all_data_from_csv(["market_stats"])["market_stats"]
-
+    
+    """ 
     max_rank = all_data['rank'].max()
     all_data['company_id'] = pd.to_numeric(all_data['company_id'], downcast='integer')
     all_data['rank'] = pd.to_numeric(all_data['rank'], downcast='integer')
     all_data['transp_ratio'] = pd.to_numeric(all_data['transp_ratio']) * 100
     all_data['comm_ratio'] = pd.to_numeric(all_data['comm_ratio']) * 100
-    all_data['actions_ratio'] = pd.to_numeric(all_data['actions_ratio']) * 100
-
-
-
+    all_data['actions_ratio'] = pd.to_numeric(all_data['actions_ratio']) * 100 """
+    """     
+    
     if not corp_number:
         corp_number = max_rank
 
     if top_rank:
         all_data = all_data.loc[all_data['rank']<=corp_number].sort_values(by=['rank'])
     else:
-        all_data = all_data.loc[all_data['rank']>max_rank - corp_number].sort_values(by=['rank'])
-
+        all_data = all_data.loc[all_data['rank']>max_rank - corp_number].sort_values(by=['rank']) 
+    
+    """
     # parsing the DataFrame in json format.
     json_records = all_data.reset_index().to_json(orient ='records')
     data = []
