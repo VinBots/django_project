4bd9f0008745c654017284eadef4d016085af5cc
Vincent Manier <vincentmanier@hotmail.com>
2022-01-03 21:12:11 -0500

    add meta data

--- market/views.py
+++ market/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 import os
-from market.utilities import get_market_stats
+from market.utilities import get_market_stats, get_meta_data
 
 # Create your views here.
 
@@ -10,5 +10,8 @@ def market_home(request):
     return render(
         request,
         "django_project/market/main.html",
-        {'market_stats':get_market_stats()}
+        {
+            'market_stats':get_market_stats(),
+            'meta': get_meta_data()
+        }
         )
