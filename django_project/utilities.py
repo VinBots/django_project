f0b9a70a876fe27e405f209d898776bfa0dc9a93
Vincent Manier <vincentmanier@hotmail.com>
2022-01-08 20:01:36 -0500

    clean up

--- django_project/utilities.py
+++ django_project/utilities.py
@@ -9,27 +9,32 @@ def get_random_logos():
 
     return Corporate.objects.random()
 
+
 def get_top10_wo_zero():
- 
+
     # Opening JSON file
-    filename = os.path.join ('django_project/static/django_project', 'data', 'top10_wo_net_zero.json')
+    filename = os.path.join('django_project/static/django_project', 'data',
+                            'top10_wo_net_zero.json')
     with open(filename) as f:
         data = json.load(f)
-    
+
     return data
+
 
 def get_top5_transp_miss_cut():
- 
+
     # Opening JSON file
-    filename = os.path.join ('django_project/static/django_project', 'data', 'top5_transp_miss_cut.json')
+    filename = os.path.join('django_project/static/django_project', 'data',
+                            'top5_transp_miss_cut.json')
     with open(filename) as f:
         data = json.load(f)
-    
+
     return data
 
+
 def get_general_stats(indicators):
-    path = os.path.join (c.DATA_FOLDER, c.TOP_STATS_FILE)
-    with open (path) as json_file:
+    path = os.path.join(c.DATA_FOLDER, c.TOP_STATS_FILE)
+    with open(path) as json_file:
         dict = json.load(json_file)
 
     return [dict[indicator] for indicator in indicators]
--- django_project/views.py
+++ django_project/views.py
@@ -5,7 +5,6 @@ from django.shortcuts import render, redirect
 from django_project.utilities import get_random_logos, get_top10_wo_zero, get_top5_transp_miss_cut
 import os
 from django.http import HttpResponse
-from pathlib import Path
 from django.core.files import File
 from django_project.utilities import get_general_stats
 from config import Config as c
@@ -13,26 +12,31 @@ from config import Config as c
 
 def home(request):
 
-  if request.GET.get("query") is not None:
-    path = reverse('corporates_home') + request.GET.get("query")
-    return redirect(path)
+    if request.GET.get("query") is not None:
+        path = reverse('corporates_home') + request.GET.get("query")
+        return redirect(path)
 
-  corporates_names = Corporate.objects.all()
-  pct_values = get_general_stats(['trust', 'commitments', 'science'])
-  angle_deg = [str(pct_value * 1.8) + "deg" for pct_value in pct_values]
+    corporates_names = Corporate.objects.all()
+    pct_values = get_general_stats(['trust', 'commitments', 'science'])
+    angle_deg = [str(pct_value * 1.8) + "deg" for pct_value in pct_values]
 
-  return render (request, "django_project/home/main.html", {
-    "corporates_names": corporates_names,
-    "random_logos": get_random_logos(),
-    "angle1":angle_deg[0],"value1":str(pct_values[0]),
-    "angle2":angle_deg[1],"value2":str(pct_values[1]),
-    "angle3":angle_deg[2],"value3":str(pct_values[2]),
-    "top5_scores": get_scores_xls(corp_number=5, top_rank=True),
-    "bottom5_scores": get_scores_xls(corp_number=5, top_rank=False),
-    "top10_wo_zero" : get_top10_wo_zero()
-    }
-      )
+    return render(
+        request, "django_project/home/main.html", {
+            "corporates_names": corporates_names,
+            "random_logos": get_random_logos(),
+            "angle1": angle_deg[0],
+            "value1": str(pct_values[0]),
+            "angle2": angle_deg[1],
+            "value2": str(pct_values[1]),
+            "angle3": angle_deg[2],
+            "value3": str(pct_values[2]),
+            "top5_scores": get_scores_xls(corp_number=5, top_rank=True),
+            "bottom5_scores": get_scores_xls(corp_number=5, top_rank=False),
+            "top10_wo_zero": get_top10_wo_zero()
+        })
 
+
+"""
 def sectors(request, sector_name):
 
   sector_data = {
@@ -43,36 +47,37 @@ def sectors(request, sector_name):
 
 def sectors_search(request):
   return render (request, "django_project/sectors/main.html")
+"""
+
 
 def aboutus(request):
-  return render (request, "django_project/aboutus/main.html")
+    return render(request, "django_project/aboutus/main.html")
+
 
 def blog(request):
 
-  return render (
-    request,
-    "django_project/blog/main.html",
-    {
-      "top5_mising_cut": get_top5_transp_miss_cut(),
-    }
-  )
+    return render(request, "django_project/blog/main.html", {
+        "top5_mising_cut": get_top5_transp_miss_cut(),
+    })
+
 
 def faq(request):
-  return render (request, "django_project/faq/main.html")
+    return render(request, "django_project/faq/main.html")
 
-def download_file (request, folder_name = '', file_name = ''):
 
-    if file_name !='':
-      #BASE_DIR_LIB = os.path.join(Path(__file__).parent.parent.parent,'net0_docs','reports')
-      BASE_DIR_LIB = os.path.join (c.DATA_FOLDER, c.LIBRARY_FOLDER)
-      filepath = os.path.join (BASE_DIR_LIB, folder_name, file_name)
-      if os.path.exists(filepath):
-        f = open(filepath, 'rb')
-        pdfFile = File(f)
-        response = HttpResponse(pdfFile.read())
-        response['Content-Disposition'] = "attachment;filename=%s" % file_name
-        return response
-      else:
-        return None
+def download_file(request, folder_name='', file_name=''):
+
+    if file_name != '':
+        BASE_DIR_LIB = os.path.join(c.DATA_FOLDER, c.LIBRARY_FOLDER)
+        filepath = os.path.join(BASE_DIR_LIB, folder_name, file_name)
+        if os.path.exists(filepath):
+            f = open(filepath, 'rb')
+            pdfFile = File(f)
+            response = HttpResponse(pdfFile.read())
+            response[
+                'Content-Disposition'] = "attachment;filename=%s" % file_name
+            return response
+        else:
+            return None
     else:
-      return redirect(reverse('main_home'))
+        return redirect(reverse('main_home'))
