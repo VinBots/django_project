efa79bb3de539ba301ae7e0e05bc1d6b722ca498
Vincent Manier <vincentmanier@hotmail.com>
2022-02-13 19:45:10 -0500

    implement models

--- corporates/urls.py
+++ corporates/urls.py
@@ -3,14 +3,14 @@ from . import views
 from .views import GHGList, GHGListCreate, GHGListUpdate
 
 urlpatterns = [
-    path("list", GHGList.as_view(), name="ghg"),
-    path("<str:corp_name>/", views.corporates_search, name="corporates_search"),
+    # path("<str:corp_name>/", views.corporates_search, name="corporates_search"),
     path("", views.corporates_home, name="corporates_home"),
     path(
         "charts/html_exports/<str:folder_name>/<str:file_name>",
         views.show_html,
         name="html_exports",
     ),
+    path("view/", GHGList.as_view(), name="ghg"),
     path("update/<int:pk>/", GHGListUpdate.as_view(), name="update-ghg"),
     path("create/", GHGListCreate.as_view(), name="create-ghg"),
 ]
