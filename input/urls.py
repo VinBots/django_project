feb049799d31452058042c4ad3aac12d9dd28b03
Vincent Manier <vincentmanier@hotmail.com>
2022-02-20 10:35:27 -0500

    user interface

--- input/urls.py
+++ input/urls.py
@@ -4,9 +4,9 @@ from .views import GHGList, GHGListCreate, GHGListUpdate
 
 urlpatterns = [
     path("ghg/view/", GHGList.as_view(), name="ghg"),
-    #path("ghg/update/<int:pk>/", GHGListUpdate.as_view(), name="update-ghg"),
+    # path("ghg/update/<int:pk>/", GHGListUpdate.as_view(), name="update-ghg"),
     path("ghg/create/", GHGListCreate.as_view(), name="create-ghg"),
     path("targets/view/", GHGList.as_view(), name="ghg"),
-    #path("targets/update/<int:pk>/", GHGListUpdate.as_view(), name="update-targets"),
+    # path("targets/update/<int:pk>/", GHGListUpdate.as_view(), name="update-targets"),
     path("targets/create/", GHGListCreate.as_view(), name="create-targets"),
 ]
