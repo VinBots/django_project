142f1acb4722ad9ad92f5a10aaa9677c4b104d70
Vincent Manier <vincentmanier@hotmail.com>
2022-02-20 12:03:46 -0500

    user interface

--- corporates/models.py
+++ corporates/models.py
@@ -1,4 +1,3 @@
-from tabnanny import verbose
 from django.db import models
 import random
 from django.db.models import Count
--- input/urls.py
+++ input/urls.py
@@ -5,7 +5,7 @@ from .views import GHGList, GHGListCreate, GHGListUpdate
 urlpatterns = [
     path("ghg/view/", GHGList.as_view(), name="ghg"),
     path("ghg/update/<int:pk>/", GHGListUpdate.as_view(), name="update-ghg"),
-    path("ghg/create/", GHGListCreate.as_view(), name="create-ghg"),
+    path("ghg/create/", GHGListCreate.as_view(), name="create-ghg")
     # path("targets/view/", GHGList.as_view(), name="ghg"),
     # path("targets/update/<int:pk>/", GHGListUpdate.as_view(), name="update-targets"),
     # path("targets/create/", GHGListCreate.as_view(), name="create-targets"),
