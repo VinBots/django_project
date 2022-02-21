95dc14f9d5116db5b8c1a435f30ecd00a6a0aef0
Vincent Manier <vincentmanier@hotmail.com>
2022-02-20 20:04:04 -0500

    user interface

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,7 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
+# from django.contrib.auth.models import AbstractUser
+# from django.db import models
 
 
-class CustomUser(AbstractUser):
+# class CustomUser(AbstractUser):
 
-    new_info = models.CharField(max_length=200, blank=True)
+#     new_info = models.CharField(max_length=200, blank=True)
