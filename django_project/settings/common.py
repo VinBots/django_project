fbd11954ebde1269b5f16ca4df9e94970216870c
Vincent Manier <vincentmanier@hotmail.com>
2022-06-20 16:23:20 -0400

    staticfiles

--- django_project/settings/common.py
+++ django_project/settings/common.py
@@ -81,9 +81,7 @@ LOGOUT_REDIRECT_URL = "home"
 
 STATIC_ROOT = os.path.join(SERVER_BASE_DIR, "static")
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"), STATIC_ROOT]
 
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
