80f28e389ebcb2916cafd581af4abce163b9ecf3
Vincent Manier <vincentmanier@hotmail.com>
2022-06-20 15:12:42 -0400

    staticfiles

--- django_project/settings/common.py
+++ django_project/settings/common.py
@@ -85,7 +85,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static", "django_project")
 #    os.path.join(BASE_DIR, 'django_project', 'static'),
 #    ]
 STATICFILES_FINDERS = [
-    # "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
 USE_THOUSAND_SEPARATOR = True
