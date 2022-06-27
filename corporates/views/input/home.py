048fb7332662c843711a767fb32b6ab90c860d3c
Vincent Manier <vincentmanier@hotmail.com>
2022-06-27 09:50:51 -0400

    user interface input status

--- corporates/views/input/home.py
+++ corporates/views/input/home.py
@@ -1,9 +1,103 @@
+from datetime import datetime, tzinfo
 from django.views.generic.list import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import redirect
 from django.urls import reverse
+from pytz import UTC
 
-from corporates.models import UserProfile, Corporate
+from corporates.models import UserProfile, Corporate, GeneralInfo
+from corporates.models.external_sources.cdp import CDP
+from corporates.models.ghg import GHGQuant
+from corporates.models.targets import TargetQuant
+from corporates.models.verification import Verification
+
+
+def days_ago(last_update):
+    today = datetime.now(tz=UTC)
+    delta = today - last_update
+    return delta.days
+
+
+def get_input_status_general(corp):
+
+    query = GeneralInfo.objects.filter(
+        company=corp,
+        year__in=["2021", "2022"],
+    ).order_by("-year", "-last_update")
+    if query.exists():
+        return query[0].year
+    else:
+        return "Missing"
+
+
+def get_input_status_disclosures(corp):
+    query = Verification.objects.filter(
+        company=corp,
+        reporting_year__in=["2021", "2022"],
+    ).order_by("-reporting_year", "-last_update")
+    if query.exists():
+        return query[0].reporting_year
+    else:
+        return "Missing"
+
+
+def get_input_status_net0(corp):
+
+    query = Verification.objects.filter(
+        company=corp,
+    ).order_by("-last_update")
+    if query.exists():
+        return f"last: {days_ago(query[0].last_update)} days ago"
+    else:
+        return "no record"
+
+
+def get_input_status_ghg(corp):
+    query = GHGQuant.objects.filter(company=corp, source="public").order_by(
+        "-reporting_year", "-last_update"
+    )
+
+    if query.exists() and query[0].last_update:
+        print(query[0].last_update)
+        msg = f"last: {days_ago(query[0].last_update)} days ago"
+        # query_year = query.filter(reporting_year__in=["2021", "2022"])
+        # if query_year.exists():
+        #     msg += f" / Recent: {query_year[0].reporting_year}"
+        return msg
+    else:
+        return "no record"
+
+
+def get_input_status_recent_ghg(corp):
+    query = GHGQuant.objects.filter(
+        company=corp, source="public", reporting_year__in=["2021", "2022"]
+    ).order_by("-reporting_year", "-last_update")
+
+    if query.exists():
+        return query[0].scope_123_loc
+    else:
+        return "no record"
+
+
+def get_input_status_targets(corp):
+    query = TargetQuant.objects.filter(
+        company=corp,
+    ).order_by("-last_update")
+    if query.exists():
+        return f"last: {days_ago(query[0].last_update)} days ago"
+    else:
+        return "no record"
+
+
+def get_input_status_cdp(corp):
+    query = CDP.objects.filter(
+        company=corp,
+        questionnaire_year__in=["2020", "2021"],
+    ).order_by("-questionnaire_year", "-last_update")
+    if query.exists():
+        return query[0].questionnaire_year
+    else:
+        return "Missing"
 
 
 class ChooseCorporateList(LoginRequiredMixin, ListView):
@@ -22,6 +116,28 @@ class ChooseCorporateList(LoginRequiredMixin, ListView):
             return redirect(path)
 
         return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        input_dict = {}
+        for corp in context["data"]:
+            input_dict[corp] = {
+                "general": get_input_status_general(corp),
+                "disclosures": get_input_status_disclosures(corp),
+                "net0": get_input_status_net0(corp),
+                "ghg": get_input_status_ghg(corp),
+                "ghg_value": get_input_status_recent_ghg(corp),
+                "targets": get_input_status_targets(corp),
+                "cdp": get_input_status_cdp(corp),
+            }
+
+        context["input_status"] = input_dict
+        return context
 
     def get_queryset(self):
 
